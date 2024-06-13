3d424b61ebca11e1bb5b444450c8795ed00c4d47
Ezequiel <ezequielalves1021@gmail.com>
2024-06-13 00:05:34 -0300

    Pointing out for backend urls

--- blog/src/Service/service.jsx
+++ blog/src/Service/service.jsx
@@ -1,26 +1,26 @@
-import { urls, urlsPRD } from "../Static/variables"
+import { urlsPRD } from "../Static/variables"
 import axios from "axios"
 
 export function getPosts(numberPage = 0){
-    return axios.get(urls.getAll, { timeout: 5000, params: { page: numberPage, size: 10 } })
+    return axios.get(urlsPRD.getAll, { timeout: 5000, params: { page: numberPage, size: 10 } })
 }
 
 export function searchPosts(value, numberPage = 0){
-    return axios.get(urls.searchPosts + value, { timeout: 5000, params: { page: numberPage, size: 10 }})
+    return axios.get(urlsPRD.searchPosts + value, { timeout: 5000, params: { page: numberPage, size: 10 }})
 }
 
 export function createPost(title, text){
-    return axios.post(urls.createPost, { title: title, text: text })
+    return axios.post(urlsPRD.createPost, { title: title, text: text })
 }
 
 export function favoritePost(id){
-    return axios.put(urls.favoritePost + id)
+    return axios.put(urlsPRD.favoritePost + id)
 }
 
 export function editPost(title, text, id){
-    return axios.put(urls.editPost + id, { title: title, text: text })
+    return axios.put(urlsPRD.editPost + id, { title: title, text: text })
 }
 
 export function removePost(id){
-    return axios.delete(urls.deletePost + id)
+    return axios.delete(urlsPRD.deletePost + id)
 }
